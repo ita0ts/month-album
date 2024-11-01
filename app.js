5290f66c0bcaaf1a1c2ecc274929aa3a19ee7044
kinko6 <rafael.alfalci@gmail.com>
2024-11-01 08:51:10 -0300

    working!!!!

--- app.js
+++
@@ -1,9 +0,0 @@
-function saveAsImage() {
-    const element = document.getElementById('content');
-    html2canvas(element).then((canvas) => {
-        const link = document.createElement('a');
-        link.href = canvas.toDataURL('image/png');
-        link.download = 'pagina_capturada.png';
-        link.click();
-    });
-}
